c605dd1c5913b1301896196d675d4743a2502db4
Alex Feng <machine12101@gmail.com>
2021-05-16 19:08:38 -0400

    Fix pepper sprite in shop

--- src/hw3/Scenes/Shop1.ts
+++ src/hw3/Scenes/Shop1.ts
@@ -11,7 +11,7 @@ export default class Shop1 extends Shop {
         this.load.image("ketchupbottle", "hw3_assets/sprites/ketchup.png");
         this.load.image("mustardbottle", "hw3_assets/sprites/mustard.png");
         this.load.image("saltgun", "hw3_assets/sprites/salt.png");
-        this.load.image("peppergun", "hw3_assets/sprites/salt.png");
+        this.load.image("peppergun", "hw3_assets/sprites/pepper.png");
 
         this.hpBuffCost = 5;
         this.speedBuffCost = 5;
