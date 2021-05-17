06cea287463eb3f0bafa3e6e59f8245a29539f76
meetAlexLau <alex.lau.1@stonybrook.edu>
2021-05-17 00:39:57 -0400

    level 2 enemies

--- src/hw3/Scenes/Level2.ts
+++ src/hw3/Scenes/Level2.ts
@@ -53,7 +53,7 @@ export default class Level2 extends GameLevel {
     }
     initializeChests(positions: Vec2[]) {
         super.initializeChests([new Vec2(2*32, 117*32), new Vec2(61*32, 117*32), new Vec2(61*32, 116*32), new Vec2(60*32, 116*32),
-            new Vec2(2*32, 69*32), new Vec2(58*32, 7*32), new Vec2(58*32, 7*32), new Vec2(30*32, 98*32), new Vec2(4*32, 71*32), new Vec2(4*32, 70*32),
+            new Vec2(2*32, 69*32), new Vec2(58*32, 7*32), new Vec2(58*32, 7*32), new Vec2(31*32, 98*32), new Vec2(4*32, 71*32), new Vec2(4*32, 70*32),
             new Vec2(4*32, 69*32), new Vec2(4*32, 68*32), new Vec2(58*32, 7*32)]);
     }
     updateScene(deltaT: number): void {
